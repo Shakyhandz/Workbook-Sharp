4df16ef34e79346dcd970bb28e6f7eb4e588054f
Alexander Autio <alexanderautio@hotmail.com>
2025-05-27 16:06:42 +0200

    refactoring

---
+++ Workbook-Sharp/Cells/CellExtensions.cs
@@ -0,0 +1,54 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace WorkbookSharp.Cells;
+
+internal static class CellExtensions
+{
+    internal static string? GetValue(this Cell? cell)
+    {
+        if (cell == null)
+            return null;
+
+        if (cell.DataType == null)
+            return cell.InnerText;
+
+        var value = cell.InnerText;
+
+        if (cell.DataType.Value == CellValues.SharedString) // For shared strings, look up the value in the shared strings table.
+        {
+            // Get worksheet from cell
+            var parent = cell?.Parent;
+
+            while (parent?.Parent != null && parent.Parent != parent && string.Compare(parent.LocalName, "worksheet", true) != 0)
+            {
+                parent = parent.Parent;
+            }
+
+            if (string.Compare(parent?.LocalName, "worksheet", true) != 0)
+            {
+                throw new Exception("Unable to find parent worksheet.");
+            }
+
+            if (parent is DocumentFormat.OpenXml.Spreadsheet.Worksheet ws &&
+                ws?.WorksheetPart?.OpenXmlPackage is SpreadsheetDocument ssDoc)
+            {
+                var sstPart = ssDoc?.WorkbookPart?.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+
+                // lookup value in shared string table
+                if (sstPart != null && sstPart.SharedStringTable != null)
+                {
+                    value = sstPart.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+                }
+            }
+        }
+        else if (cell.DataType.Value == CellValues.Boolean)
+        {
+            value = value == "0" || value.Equals("false", StringComparison.OrdinalIgnoreCase)
+                    ? "false"
+                    : "true";
+        }
+
+        return value;
+    }
+}
--- Workbook-Sharp/Internal/ExcelParser.cs
+++ Workbook-Sharp/Internal/ExcelParser.cs
@@ -65,7 +65,7 @@ internal class ExcelParser : IExcelParser, IDisposable
         {
             if (c.CellReference == currentAddress)
             {
-                var header = GetCellValue(c)?.Replace(" ", "").ToUpper() ?? ("Col_" + column);
+                var header = c.GetValue()?.Replace(" ", "").ToUpper() ?? ("Col_" + column);
                 mHeaders.Add(header, headerIndex++);
                 currentAddress = CellReference.GetColumnName(++column) + HeaderRow.ToString();
 
@@ -86,7 +86,7 @@ internal class ExcelParser : IExcelParser, IDisposable
                 for (uint col = HeaderStartColumn; col < HeaderStartColumn + mHeaders.Count; col++)
                 {
                     var c = row.Elements<Cell>().Where(x => x.CellReference == CellReference.GetColumnName(col) + currentRow.ToString()).FirstOrDefault();
-                    cells.Add(GetCellValue(c) ?? "");
+                    cells.Add(c.GetValue() ?? "");
                 }
 
                 // Break if empty row
@@ -102,53 +102,6 @@ internal class ExcelParser : IExcelParser, IDisposable
         }
     
         yield break;
-    }
-
-    private string? GetCellValue(Cell? cell)
-    {
-        if (cell == null)
-            return null;
-
-        if (cell.DataType == null)
-            return cell.InnerText;
-
-        var value = cell.InnerText;
-
-        if (cell.DataType.Value == CellValues.SharedString) // For shared strings, look up the value in the shared strings table.
-        {
-            // Get worksheet from cell
-            var parent = cell?.Parent;
-
-            while (parent?.Parent != null && parent.Parent != parent && string.Compare(parent.LocalName, "worksheet", true) != 0)
-            {
-                parent = parent.Parent;
-            }
-
-            if (string.Compare(parent?.LocalName, "worksheet", true) != 0)
-            {
-                throw new Exception("Unable to find parent worksheet.");
-            }
-
-            if (parent is DocumentFormat.OpenXml.Spreadsheet.Worksheet ws &&
-                ws?.WorksheetPart?.OpenXmlPackage is SpreadsheetDocument ssDoc)
-            {
-                var sstPart = ssDoc?.WorkbookPart?.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
-
-                // lookup value in shared string table
-                if (sstPart != null && sstPart.SharedStringTable != null)
-                {
-                    value = sstPart.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
-                }
-            }
-        }
-        else if (cell.DataType.Value == CellValues.Boolean)
-        {
-            value = value == "0" || value.Equals("false", StringComparison.OrdinalIgnoreCase)
-                    ? "false"
-                    : "true";
-        }
-
-        return value;
     }
 
     private void Init()
